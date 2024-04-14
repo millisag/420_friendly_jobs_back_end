5ef7050cca1bbc18615e3a95edcdc8ae5a5eff06
Millisa G <135849424+millisag@users.noreply.github.com>
2024-04-14 19:19:59 -0400

    Added rspec tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,30 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_03_20_224233) do
+  create_table "employers", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "job_applications", force: :cascade do |t|
+    t.integer "job_id", null: false
+    t.integer "job_seeker_id", null: false
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["job_id"], name: "index_job_applications_on_job_id"
+    t.index ["job_seeker_id"], name: "index_job_applications_on_job_seeker_id"
+  end
+
+  create_table "job_seekers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "jobs", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -20,6 +44,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_20_224233) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "company_id"
+  end
+
+  create_table "saved_jobs", force: :cascade do |t|
+    t.integer "job_id", null: false
+    t.integer "job_seeker_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["job_id"], name: "index_saved_jobs_on_job_id"
+    t.index ["job_seeker_id"], name: "index_saved_jobs_on_job_seeker_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +67,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_20_224233) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "job_applications", "job_seekers"
+  add_foreign_key "job_applications", "jobs"
+  add_foreign_key "saved_jobs", "job_seekers"
+  add_foreign_key "saved_jobs", "jobs"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with a username and password' do
+    user = build(:user)
+    expect(user).to be_valid
+  end
+
+  it 'is not valid without a username' do
+    user = build(:user, username: nil)
+    expect(user).not_to be_valid
+  end
+
+  it 'hashes the password' do
+    user = create(:user, password: 'password')
+    expect(user.password_digest).not_to eq 'password'
+  end
 end
